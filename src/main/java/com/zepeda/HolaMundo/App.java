a029514d81537a1fc5caeaf420eb2418369edc56
Zepeda <61017335+JuMaze@users.noreply.github.com>
2020-04-22 10:38:41 -0500

    test fot gitignore file

---
+++ src/main/java/com/zepeda/HolaMundo/App.java
@@ -0,0 +1,13 @@
+package com.zepeda.HolaMundo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
